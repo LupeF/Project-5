f47fac7d0895a9d1c6b0c2444f153c3a19e45cff
LupeF <46634202+LupeF@users.noreply.github.com>
2020-08-24 15:05:24 -0700

    add partials and js

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// const search = new Filter('search', 'data-caption');
+
+
+//plug in
+baguetteBox.run('.gallery');
+
+
+search
+document.querySelector('#search').addEventListener('keyup',function(){
+
+    const input = document.querySelector('#search').value.toLowerCase();
+    const images = document.querySelectorAll('a');
+
+    for (let i = 0;  i < images.length; i++){
+        //match description
+        const captions = document.querySelectorAll('a')[i].getAttribute('data-caption').toLowerCase();
+        //show picture
+        if (captions.includes(input)){
+            $(document.querySelectorAll('a')[i]).show();
+        //hide picture
+        }else
+            $(document.querySelectorAll('a')[i]).hide();
+    }
+});
